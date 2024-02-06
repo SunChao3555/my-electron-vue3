5724632d7ebb23241765ee900d9f9967e037e878
SunChao3555 <35911336+SunChao3555@users.noreply.github.com>
2024-02-06 16:35:55 +0800

    Initial commit

---
+++ src/main/windows/primary/index.ts
@@ -0,0 +1,85 @@
+import path from "node:path";
+import { BrowserWindow, app, ipcMain } from "electron";
+import appState from "../../app-state";
+import WindowBase from "../window-base";
+import FramelessWindow from "../frameless";
+
+class MainWindow extends WindowBase{
+  protected createWindow() : BrowserWindow | null{
+    const mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, "preload.js"),
+        nodeIntegration: false,
+        contextIsolation: true,
+      },
+    });
+  
+    mainWindow.on("close", (e) => {
+      if(!appState.willExitApp && appState.minToTrayWhenClose){
+        mainWindow.hide();
+        if(!appState.cfgStore?.get("TrayBalloonDisplayed", false) as boolean){
+          appState.cfgStore?.set("TrayBalloonDisplayed", true);
+          if(appState.tray){
+            appState.tray.displayBalloon({
+              title: "electron-vue-template",
+              content: "The program has been minimized to the system tray.\n\nThis balloon will only be displayed once!"
+            });
+          }
+        }
+        e.preventDefault();
+      }
+    });
+  
+    if(process.env.NODE_ENV === "development"){
+      const rendererPort = process.argv[2];
+      mainWindow.loadURL(`http://localhost:${rendererPort}/pages/primary/index.html`);
+    }else{
+      mainWindow.loadFile(path.join(app.getAppPath(), "build/renderer/pages/primary/index.html"));
+    }
+  
+    return mainWindow;
+  }
+
+  protected registerIpcMainHandler(): void{
+    ipcMain.on("message", (event, message) => {
+      if(!this.isIpcMainEventBelongMe(event))
+        return;
+
+      console.log(message);
+    });
+  
+    ipcMain.on("show-frameless-sample-window", (event) => {
+      if(!this.isIpcMainEventBelongMe(event))
+        return;
+
+      if(appState.framelessWindow?.valid){
+        appState.framelessWindow?.browserWindow?.show();
+      }else{
+        appState.framelessWindow = new FramelessWindow();
+      }
+    });
+
+    ipcMain.on("clear-app-configuration", (event) => {
+      if(!this.isIpcMainEventBelongMe(event))
+        return;
+      appState.cfgStore?.clear();
+    });
+    
+    function delay(time){
+      return new Promise(resolve => setTimeout(resolve, time));
+    }
+    
+    ipcMain.handle("async-exit-app", async(event) => {
+      if(!this.isIpcMainEventBelongMe(event))
+        return;
+
+      await delay(1500);
+      appState.willExitApp = true;
+      app.quit();
+    });
+  }
+}
+
+export default MainWindow;
---
+++ src/main/windows/primary/preload.ts
@@ -0,0 +1,15 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+/*
+The preload for MainWindow
+*/
+contextBridge.exposeInMainWorld("electronAPI", {
+  sendMessage: (message: string) => ipcRenderer.send("message", message),
+  showFramelessSampleWindow: () => ipcRenderer.send("show-frameless-sample-window"),
+  openExternalLink: (url: string) => ipcRenderer.send("open-external-link", url),
+  clearAppConfiguration: () => ipcRenderer.send("clear-app-configuration"),
+  onShowExitAppMsgbox: (callback) => ipcRenderer.on("show-exit-app-msgbox", () => {
+    callback();
+  }),
+  asyncExitApp: () => ipcRenderer.invoke("async-exit-app"),
+});
